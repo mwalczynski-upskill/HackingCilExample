7068925b41a925cf9ab67c77ee052bd7843ff02b
Michał Walczyński <218544@student.pwr.edu.pl>
2018-01-10 19:46:27 +0100

    Hacking IL Example

---
+++ HackingCilExample.cs
@@ -0,0 +1,21 @@
+using System;
+namespace HackingCilExample
+{
+    class HackingCilExample
+    {
+        static void Main()
+        {
+            HelloFromCode();
+        }
+
+        static void HelloFromCode()
+        {
+            Console.WriteLine("Hello from VS Code!");
+        }
+
+        static void HelloFromIl()
+        {
+            Console.WriteLine("Hello from Il!");
+        }
+    }
+}
